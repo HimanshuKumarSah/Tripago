a6279010b780a53c95f3da7d0d19d8d1b156ab17
jai <jaigaurav157@gmail.com>
2024-03-16 22:37:56 +0530

    Updated form logic
    form data is now assigned to variables and then assigned to the corresponding form class object instead of being done directly
    
    Signed-off-by: jai <jaigaurav157@gmail.com>

--- backend/tripago/forms.py
+++ backend/tripago/forms.py
@@ -3,7 +3,7 @@ from django.contrib.auth.models import User
 from django import forms
 from django.forms.widgets import PasswordInput, TextInput
 
-# Register a user
+# register a user
 class CreateUserForm(UserCreationForm):
     class Meta:
         model = User
@@ -15,7 +15,13 @@ class CreateUserForm(UserCreationForm):
                   'password2'
                   ]
         
-# Authenticate a user
+# authenticate a user
 class LoginForm(AuthenticationForm):
-    username = forms.CharField(widget=TextInput(attrs={'class': 'form-control', 'placeholder': 'Username'}))
-    password = forms.CharField(widget=PasswordInput(attrs={'class': 'form-control', 'placeholder': 'Password'}))
+    username = forms.CharField(widget=TextInput())
+    password = forms.CharField(widget=PasswordInput())
+
+
+# create an itinerary
+class ItineraryGenerationForm(forms.Form):
+    city = forms.CharField(widget=TextInput())
+    country = forms.CharField(widget=TextInput())
--- backend/tripago/views.py
+++ backend/tripago/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
-from . forms import CreateUserForm, LoginForm
+from . forms import CreateUserForm, LoginForm, ItineraryGenerationForm
 from django.contrib.auth.decorators import login_required
+from django.http import HttpResponse
 
 # authentication models and functions
 from django.contrib.auth.models import auth
@@ -15,36 +16,52 @@ def register(request):
     form = CreateUserForm()
     
     if request.method == "POST":
-        form = CreateUserForm(request.POST)
+        first_name = request.POST.get('first_name')
+        last_name = request.POST.get('last_name')
+        username = request.POST.get('username')
+        email = request.POST.get('email')
+        password1 = request.POST.get('password1')
+        password2 = request.POST.get('password2')
+
+        form = CreateUserForm(data = {
+            'first_name': first_name,
+            'last_name': last_name,
+            'username': username,
+            'email': email,
+            'password1': password1,
+            'password2': password2
+        })
+
         if form.is_valid():
             form.save()
 
             return redirect('login')
-    
-    context = {'registerform': form}
 
-    return render(request, 'tripago/register.html', context=context)
+    return render(request, 'tripago/register.html')
 
 
 def login(request):
     form = LoginForm()
 
     if request.method == "POST":
-        form = LoginForm(request, data=request.POST)
-        if form.is_valid():
-            username = request.POST.get('username')
-            password = request.POST.get('password')
+        username = request.POST.get('username')
+        password = request.POST.get('password')
 
+        form = LoginForm(request, data={
+            'username': username,
+            'password': password
+        
+        })
+
+        if form.is_valid():
             user = authenticate(request, username=username, password=password)
 
             if user is not None:
                 auth.login(request, user)
 
                 return redirect('dashboard')
-    
-    context = {'loginform': form}
 
-    return render(request, 'tripago/login.html', context=context)
+    return render(request, 'tripago/login.html')
 
 
 @login_required(login_url='login')
@@ -55,3 +72,20 @@ def dashboard(request):
 def logout(request):
     auth.logout(request)
     return redirect("")
+
+
+def create_itinerary(request):
+    form = ItineraryGenerationForm()
+
+    if request.method == "POST":
+        city = request.POST.get('city')
+        country = request.POST.get('country')
+
+        form = ItineraryGenerationForm(data={
+            'city': city, 
+            'country': country
+        })
+
+        return HttpResponse(f"Your itinerary for {city} in {country} has been created successfully!")
+
+    return render(request, 'tripago/create-itinerary.html')
